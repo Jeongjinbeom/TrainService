105d6c895448f78af69623ec07b4f8330b64f951
Jeongjinbeom <wlsqjal12345@naver.com>
2024-08-06 13:47:17 +0900

    mini_Train

---
+++ src/users/service/Main.java
@@ -0,0 +1,10 @@
+package users.service;
+
+public class Main {
+    public static void main(String[] args) {
+        TrainService trainService = new TrainService();
+        trainService.menu();
+ 
+    }
+
+}
